dce7981cc468e474a0684c8172cbaa1f4e7b3880
Chuckie Granada <devchuckcamp@gmail.com>
2019-05-23 22:38:03 +0800

    staging build #21

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -26,7 +26,7 @@ export class GlobalRoutesService {
     public mimsPort='80';
     public apiPort='';
     //public apiPort = '8081'; //for staging
-    public host='5d168f15.ngrok.io'; //development
+    public host='5fb1bfde.ngrok.io'; //development
     //public host='192.168.10.10'; //for staging
     //public host='support.ecomia.com'; //for production
     //public protocol='http://'; //for development
