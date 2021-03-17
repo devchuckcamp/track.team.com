9692ec2d941f70c1369d669b1e8b9c7df355376f
chuckie <devchuckcamp@gmail.com>
2021-03-17 16:56:28 -0500

    ticket author added on ticket view

--- src/app/config/client.ts
+++ src/app/config/client.ts
@@ -4,10 +4,11 @@ import { map, take } from 'rxjs/operators';
 @Injectable()
 export class ClientGlobalRoutesService {
 
-    private clientID = 'newclient';
+    //private clientID = 'newclient';
     //private storage = localStorage.getItem('currentUser').json();
     private Bearer:any;
     private clientSecret = "";
+    private clientID:number = 0;
 
     constructor() {
         if(localStorage.getItem("currentUser")){
@@ -15,14 +16,18 @@ export class ClientGlobalRoutesService {
         }
 
 
-
         
-        //Development Key
-            this.clientSecret = "qQw77McftRqbYgFdFPaohqcBtEGi5Hf0F53kD1HZ";
+        //Development Client ID
+            //this.clientID = 6;
+        //Production Production
+            this.clientID = 2;
+        
+        //Development Key 
+           //this.clientSecret = "cxRsFvBb9NSocKZsan6fjDPTs6NRyTe7t9YUGxHE";
         //Production Key
-            //this.clientSecret = "daQUm35rekLrDeRoMdlTCyjbrOrTHvoev107mhdO";
+            this.clientSecret = "qQw77McftRqbYgFdFPaohqcBtEGi5Hf0F53kD1HZ";
 
-        //2nd Key
+        //2nd Key 
             //this.clientSecret   =   "cYoSGSGh9CVwemD0uwKeFztSNB9e75v5VbdIRh2f";
         //Production Key
             //this.clientSecret = "MpVC9NbhB3WcoozfvUICL1Mg14ADD7nfQy1eGGuB";
@@ -32,7 +37,7 @@ export class ClientGlobalRoutesService {
     
     getClientKey(){
         var data = JSON.stringify({
-            "client_id" :2,
+            "client_id" :this.clientID,
             "client_secret":this.clientSecret,
             "grant_type" : "password",
         })
